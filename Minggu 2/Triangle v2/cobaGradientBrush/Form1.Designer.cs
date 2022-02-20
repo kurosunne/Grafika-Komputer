3136124210afc09f4cdcee8b0ce07c9cb347bbb6
mikhaelc09 <mikhael.chris09@gmail.com>
2022-02-20 17:40:17 +0700

    make triangle c# v2

---
+++ Minggu 2/Triangle v2/cobaGradientBrush/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace cobaGradientBrush {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.c1 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // c1
+            // 
+            this.c1.Location = new System.Drawing.Point(665, 49);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(97, 40);
+            this.c1.TabIndex = 1;
+            this.c1.UseVisualStyleBackColor = true;
+            this.c1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // c2
+            // 
+            this.c2.Location = new System.Drawing.Point(665, 127);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(97, 40);
+            this.c2.TabIndex = 1;
+            this.c2.UseVisualStyleBackColor = true;
+            this.c2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // c3
+            // 
+            this.c3.Location = new System.Drawing.Point(665, 202);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(97, 40);
+            this.c3.TabIndex = 1;
+            this.c3.UseVisualStyleBackColor = true;
+            this.c3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(665, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Color 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(665, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Color 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(665, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Color 3:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Minggu 2/Triangle v2/cobaGradientBrush/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace cobaGradientBrush {
+    public partial class Form1 : Form {
+        Point[] click = new Point[3];
+        Button[] cp;
+        int ctr = 0;
+        public Form1() {
+            InitializeComponent();
+            cp = new Button[3]{c1, c2, c3};
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if(colorDialog1.ShowDialog() == DialogResult.OK) {
+                ((Button)sender).BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void drawTriangle(Point[] pt, Color[] col, Graphics g) {
+            TrianglePoint[] tp = new TrianglePoint[3];
+            tp[0] = new TrianglePoint(pt[0], col[0]);
+            tp[1] = new TrianglePoint(pt[1], col[1]);
+            tp[2] = new TrianglePoint(pt[2], col[2]);
+
+            PathGradientBrush path = new PathGradientBrush(pt);
+
+            path.SurroundColors = col;
+
+            path.CenterColor = getCenterColor(tp);
+
+            g.FillPolygon(path, pt);
+        }
+
+        private Color getCenterColor (TrianglePoint[] triangle){
+            int[] cc = new int[3];
+            for (int i = 0; i < 3; i++) {
+                int[] a = MatrixOperator.getVector(triangle[0].getMatrix(i), triangle[1].getMatrix(i));
+                int[] b = MatrixOperator.getVector(triangle[1].getMatrix(i), triangle[2].getMatrix(i));
+                int[] res = MatrixOperator.crossProduct(a, b);
+                int D = -(
+                        res[0] * triangle[0].getMatrix(i)[0] +
+                        res[1] * triangle[0].getMatrix(i)[1] +
+                        res[2] * triangle[0].getMatrix(i)[2]
+                    );
+                int[] tricenter = getTriangleCenterPoint(triangle);
+                cc[i] = Math.Abs((res[0] * tricenter[0] + res[1] * tricenter[1] + D) / res[2]);
+            }
+            for (int i = 0; i < cc.Length; i++) {
+                Console.WriteLine(cc[i]);
+            }
+            return Color.FromArgb(255, cc[0], cc[1], cc[2]);
+        }
+
+        private int[] getTriangleCenterPoint(TrianglePoint[] triangle, int idx = 0) {
+            return new int[] {
+                (triangle[0].getMatrix(idx)[0] + triangle[1].getMatrix(idx)[0] + triangle[2].getMatrix(idx)[0])/3,
+                (triangle[0].getMatrix(idx)[1] + triangle[1].getMatrix(idx)[1] + triangle[2].getMatrix(idx)[1])/3
+                };
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e) {
+            Graphics g = pictureBox1.CreateGraphics();
+            click[ctr] = new Point(e.X, e.Y);
+            g.FillRectangle(new SolidBrush(cp[ctr].BackColor), e.X, e.Y, 1, 1);
+            ctr++;
+            if(ctr == 3) {
+                ctr = 0;
+                drawTriangle(setTrianglePosition(click), new Color[] { cp[0].BackColor, cp[1].BackColor, cp[2].BackColor }, g);
+            }
+        }
+
+        private Point[] setTrianglePosition(Point[] p) {
+            int mostLeftVal = Math.Min(p[0].X, Math.Min(p[1].X , p[2].X));
+            //int mostBottomVal = Math.Max(p[0].Y, Math.Max(p[1].Y, p[2].Y));
+            //int mostRightVal = Math.Max(p[0].X, Math.Max(p[1].X, p[2].X));
+            Point pMostLeft = (p[0].X == mostLeftVal) ? p[0] : (p[1].X == mostLeftVal) ? p[1] : p[2];
+            //Point pMostBottom= (p[0].Y == mostBottomVal) ? p[0] : (p[1].Y == mostBottomVal) ? p[1] : p[2];
+            //Point pMostRight = (p[0].X == mostRightVal) ? p[0] : (p[1].X == mostRightVal) ? p[1] : p[2];
+            Point p1, p2, p3;
+            p1 = pMostLeft;
+            if (p1 == p[0]) {
+                if(p[1].X > p[2].X) {
+                    if(p[1].Y > p[2].Y) {
+                        p2 = p[2];
+                        p3 = p[1];
+                    }
+                    else {
+                        p2 = p[1];
+                        p3 = p[2];
+                    }
+                }
+                else {
+                    if (p[1].Y > p[2].Y) {
+                        p2 = p[1];
+                        p3 = p[2];
+                    }
+                    else {
+                        p2 = p[2];
+                        p3 = p[1];
+                    }
+                }
+            }
+            else if (p1 == p[1]) {
+                if (p[0].X > p[2].X) {
+                    if (p[0].Y > p[2].Y) {
+                        p2 = p[2];
+                        p3 = p[0];
+                    }
+                    else {
+                        p2 = p[0];
+                        p3 = p[2];
+                    }
+                }
+                else {
+                    if (p[0].Y > p[2].Y) {
+                        p2 = p[0];
+                        p3 = p[2];
+                    }
+                    else {
+                        p2 = p[2];
+                        p3 = p[0];
+                    }
+                }
+            }
+            else {
+                if (p[1].X > p[0].X) {
+                    if (p[1].Y > p[0].Y) {
+                        p2 = p[0];
+                        p3 = p[1];
+                    }
+                    else {
+                        p2 = p[1];
+                        p3 = p[0];
+                    }
+                }
+                else {
+                    if (p[1].Y > p[0].Y) {
+                        p2 = p[1];
+                        p3 = p[0];
+                    }
+                    else {
+                        p2 = p[0];
+                        p3 = p[1];
+                    }
+                }
+            }
+            return new Point[] { p1,p2,p3};
+        }
+
+    }
+
+    class TrianglePoint {
+        Point coord;
+        Color color;
+        public TrianglePoint(Point coord, Color color) {
+            this.color = color;
+            this.coord = coord;
+        }
+
+        public TrianglePoint(int x, int y, int a, int r, int g, int b) {
+            this.coord = new Point(x, y);
+            this.color = Color.FromArgb(a, r, g, b);
+        }
+
+        public int[] getMatrix(int code) {
+            if(code == 0) {
+                return new int[]{ coord.X, coord.Y, color.R};
+            }
+            else if(code == 1) {
+                return new int[]{ coord.X, coord.Y, color.G};
+            }
+            else if(code == 2) {
+                return new int[]{ coord.X, coord.Y, color.B};
+            }
+            else {
+                return null;
+            }
+        }
+    }
+
+    class MatrixOperator {
+        public static int[] crossProduct(int[] m1, int[] m2) {
+            return new int[] { 
+                Math.Abs(m1[1]*m2[2] - m1[2]*m2[1]),
+                Math.Abs(m1[2]*m2[0] - m1[0]*m2[2]),
+                Math.Abs(m1[0]*m2[1] - m1[1]*m2[0])
+            };
+        }
+
+        public static int[] getVector(int[] m1, int[] m2) {
+            return new int[] { 
+                m2[0] - m1[0],
+                m2[1] - m1[1],
+                m2[2] - m1[2] 
+            };
+        }
+    }
+}
